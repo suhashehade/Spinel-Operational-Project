43bbc89ee5fa651e345705d1f9fb190291f084fd
Suha523 <suhashehadeh123@gmail.com>
2024-05-28 15:00:25 +0300

    latest version

--- lib/common/logos/spinel_light_logo_widget.dart
+++ lib/common/logos/spinel_light_logo_widget.dart
@@ -3,9 +3,6 @@ import 'package:operations/utils/assets.dart';
 
 class SpinelLogoWidget extends StatelessWidget {
   const SpinelLogoWidget({super.key});
-  final double top = 0.0; // 270
-  final double left = 0.0; // 130
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
